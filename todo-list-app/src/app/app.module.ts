f2258f077bbf1fd45ddd16a94aa257df7dafb471
Pooja Rani <pooja.rani@hashedin.com>
2020-08-14 20:35:37 +0530

    Created Child Component

--- todo-list-app/src/app/app.module.ts
+++ todo-list-app/src/app/app.module.ts
@@ -5,15 +5,31 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 //import { environment } from '../environments/environment';
+
+import { MatSliderModule } from '@angular/material/slider';
+import { MatTableModule } from '@angular/material/table';
+import { TodoComponent } from './components/todo/todo.component';
+import { DataTableComponent } from './data-table/data-table.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    DataTableComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
     // AngularFireModule.initializeApp(environment.firebase),
     // AngularFireDatabaseModule
   ],
